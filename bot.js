ceb538ee29694c384b68c5e8420589d0dca60190
JuantloGT <35728606+JuantloGT@users.noreply.github.com>
2018-01-24 16:17:38 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,5 +30,12 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === '!bbni') {
+        //privatemessage
+    	message.author.sendMessage("www.instagram.com/bbnifx")
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
