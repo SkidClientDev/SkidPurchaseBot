3a8c4d8131b8dbba0fb64bc3e17d19d47928d3c6
SkidClientDev <36345990+SkidClientDev@users.noreply.github.com>
2018-02-11 07:45:51 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('BBNI BOT HAS BEEN TURNED ON! MADE BY BBNIGT!');
+    console.log('SkidPurchaseBot Its Ready~!');
     
   
 });
@@ -11,19 +11,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === '!help') {
-    	message.reply('Help!');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === '!website') {
-    	message.reply('https://bbni.ml');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === '!bot') {
-    	message.reply('Online');
+    	message.reply('!buy');
   	}
 });
 
@@ -35,34 +23,10 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if (message.content === '!order') {
+    if (message.content === '!buy') {
         //privatemessage
-    	message.author.sendMessage("Welcome To BBNI Order Panel!\nPlease Do !order intro If You Want To Buy Intro!.")
+    	message.author.sendMessage("Skid Its Currently Not For Sale!!.")
   	}
 });
-
-client.on('message', message => {
-    if (message.content === '!order intro') {
-        //privatemessage
-    	message.author.sendMessage("To Order An Intro Go To This Website: https://bbni.ml")
-  	}
-});
-
-
-client.on('message', message => {
-    if (message.content === '!bbni') {
-        //privatemessage
-    	message.author.sendMessage("www.instagram.com/bbnifx")
-  	}
-});
-
-
-client.on('message', message => {
-    if (message.content === '!invite') {
-        //privatemessage
-    	message.author.sendMessage("www.discord.io/bbnialt")
-  	}
-});
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
