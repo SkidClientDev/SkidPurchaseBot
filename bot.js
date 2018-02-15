5109dcecc44bbca89b49e7b82e7751a8f49c4843
SkidClientDev <36345990+SkidClientDev@users.noreply.github.com>
2018-02-15 08:36:06 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('SkidPurchaseBot Its Ready~!');
     client.user.setStatus('Playing SkidClient')
-  
+    client.setStatus('Playing SkidClient')
 });
 
 
